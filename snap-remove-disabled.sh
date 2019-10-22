916ca5e0408f446a3d7cf5405f43060fce6de8fb
fidoedidoe <gmf@pebbleglow.co.uk>
2019-10-22 16:31:17 +0100

    Add runtime check for root user

---
+++ snap-remove-disabled.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+
+# instruct bash to: 
+#  -e "fail on non zero exit status"
+#  -u "fail on non-defined variables"
+#  -o pipefail "prevent errors in pipeline from being masked"
+set -euo pipefail
+
+# Check for root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done

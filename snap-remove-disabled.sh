3cf1fabbd59f428521c05f1c581c3df15a6e0da7
fidoedidoe <gmf@pebbleglow.co.uk>
2019-10-23 11:07:02 +0100

    Made standard output more verbose

--- snap-remove-disabled.sh
+++ snap-remove-disabled.sh
@@ -14,6 +14,8 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+snap list --all
+
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
         snap remove "$snapname" --revision="$revision"

4965f8556ba3417c069a400adb518a6655047d66
fidoedidoe <gmf@pebbleglow.co.uk>
2019-11-12 11:18:46 +0000

    refined echo'd output to screen

--- snap-remove-disabled.sh
+++ snap-remove-disabled.sh
@@ -16,7 +16,9 @@ fi
 
 snap list --all
 
+echo ""
+echo ""
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
-        snap remove "$snapname" --revision="$revision"
+       echo -n "removing $snapname: rev - $revision....." && snap remove "$snapname" --revision="$revision"
     done

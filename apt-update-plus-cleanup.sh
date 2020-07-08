7096b6ae4777e91a10374694839a070d6a64c46c
fidoedidoe <gmf@pebbleglow.co.uk>
2020-07-08 17:04:25 +0100

    Add conditional check for installation of snap

--- apt-update-plus-cleanup.sh
+++ apt-update-plus-cleanup.sh
@@ -11,6 +11,10 @@
 
 set -euo pipefail
 
+# variables: 
+FILE=/usr/bin/snap
+UBUNTU_VERSION=$(lsb_release -ds)
+
 # Check for root
 if [ "$EUID" -ne 0 ]
   then echo "apt-update-plus-cleanup.sh: Please run as root"
@@ -34,20 +38,27 @@ echo "apt: complete!"
 echo "==="
 echo ""
 echo ""
-echo "=========="
-echo "snap store: checking for updates..."
-echo "=========="
-snap refresh
-echo "=========="
-echo "snap store: listing apps..."
-echo "=========="
-snap list --all
+
+#is snapd/snapd installed?
+if [ -f "$FILE" ]; then
+   echo "=========="
+   echo "snap store: checking for updates..."
+   echo "=========="
+   snap refresh
+   echo "=========="
+   echo "snap store: listing apps..."
+   echo "=========="
+   snap list --all
+   echo ""
+   echo ""
+   LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+       while read snapname revision; do
+          echo -n "removing $snapname: rev - $revision....." && snap remove "$snapname" --revision="$revision"
+       done
+fi
 
 echo ""
 echo ""
-LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
-       echo -n "removing $snapname: rev - $revision....." && snap remove "$snapname" --revision="$revision"
-    done
-echo "Bodhi is now up to date!"
+
+echo "$UBUNTU_VERSION is now up to date!"
 read -p "Press [Enter] to close this window $SUDO_USER."

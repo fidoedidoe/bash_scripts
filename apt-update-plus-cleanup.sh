fbe73f7500f189e044ae77d0012c0ff40a348c27
fidoedidoe <gmf@pebbleglow.co.uk>
2020-07-16 11:39:59 +0100

    Cosmetic: improved stdout messaging

--- apt-update-plus-cleanup.sh
+++ apt-update-plus-cleanup.sh
@@ -17,72 +17,94 @@ SNAP=/usr/bin/snap
 FLATPAK=/usr/bin/flatpak
 UBUNTU_VERSION=$(lsb_release -ds)
 
+# functions
+echoMsg() {
+
+  YELLOW='\033[1;33m'
+  NC='\033[0m' # No Color
+ 
+  MSG=${1:-}
+  ARG1=${2:-}
+
+  eval 'echo -e $ARG1 $YELLOW$MSG$NC'
+}
+
 # Check for root
 if [ "$EUID" -ne 0 ]
-  then echo "apt-update-plus-cleanup.sh: Please run as root"
+  then echoMsg "apt-update-plus-cleanup.sh: Please run as root"
   exit
 fi
 
-echo "==="
-echo "apt: updating $UBUNTU_VERSION repositories..."
-echo "==="
+echoMsg "==="
+echoMsg "apt: starting..."
+echoMsg "apt: updating $UBUNTU_VERSION repositories..."
+echoMsg "==="
 apt update
-echo "==="
-echo "apt: checking for $UBUNTU_VERSION updates..."
-echo "==="
+echoMsg "==="
+echoMsg "apt: checking for updates in refreshed repositories..."
+echoMsg "==="
 apt dist-upgrade
-echo "==="
-echo "apt: remove obsolete dependancies for $UBUNTU_VERSION..."
-echo "==="
+echoMsg "==="
+echoMsg "apt: removing obsolescence..."
+echoMsg "==="
 apt autoremove && apt autoclean
-echo "==="
-echo "apt: $UBUNTU_VERSION updates complete!"
-echo "==="
+echoMsg "==="
+echoMsg "apt: finished!"
+echoMsg "==="
 echo ""
 echo ""
 
 #is snap installed?
 if [ -f "$SNAP" ]; then
-   echo "=========="
-   echo "snap store: checking for updates..."
-   echo "=========="
+   echoMsg "=========="
+   echoMsg "snap store: starting"
+   echoMsg "snap store: checking for updates..."
+   echoMsg "=========="
    snap refresh
-   echo "=========="
-   echo "snap store: listing apps..."
-   echo "=========="
+   echoMsg "=========="
+   echoMsg "snap store: listing apps..."
+   echoMsg "=========="
    snap list --all
    echo ""
    echo ""
    LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
        while read snapname revision; do
-          echo -n "removing $snapname: rev - $revision....." && snap remove "$snapname" --revision="$revision"
+          echoMsg "removing $snapname: rev - $revision....." && snap remove "$snapname" --revision="$revision" -n
        done
+   echoMsg "=========="
+   echoMsg "snap store: finished!"
+   echoMsg "=========="
+
 else
-   echo "====="
-   echo "snapd: not installed, skipping..."
-   echo "====="
+   echoMsg "====="
+   echoMsg "snapd: is not installed, skipping..."
+   echoMsg "====="
 fi
 
 #is flatpak installed?
 if [ -f "$FLATPAK" ]; then
-   echo "======="
-   echo "flatpak: checking for updates..."
-   echo "======="
+   echoMsg "======="
+   echoMsg "flatpak: starting..."
+   echoMsg "flatpak: checking for updates..."
+   echoMsg "======="
    flatpak update
-   echo "======="
-   echo "flatpak: listing apps..."
-   echo "======="
+   echoMsg "======="
+   echoMsg "flatpak: listing apps..."
+   echoMsg "======="
    flatpak list
-   echo ""
-   echo ""
+   echoMsg "======="
+   echoMsg "flatpak: finished!"
+   echoMsg "======="
 else
-   echo "====="
-   echo "flatpak: not installed, skipping..."
-   echo "====="
+   echoMsg "====="
+   echoMsg "flatpak: is not installed, skipping..."
+   echoMsg "====="
 fi
 
 echo ""
 echo ""
 
-echo "$UBUNTU_VERSION is now up to date!"
-read -p "Press [Enter] to close this window $SUDO_USER."
+echoMsg "================="
+echoMsg "Process complete! $UBUNTU_VERSION is now up to date! Press [Enter] to close this window $SUDO_USER."
+echoMsg "================="
+read -p ""
